eb30fa9ad76693f4fbafafc22383536cd2b34f49
Jackson Berbette <jberbs321@gmail.com>
2024-10-19 12:40:16 -0400

    added resource file with style sheet

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // Load an application style
+    QFile styleFile( ":/style.qss" );
+    styleFile.open( QFile::ReadOnly );
+
+    // Apply the loaded stylesheet
+    QString style( styleFile.readAll() );
+    a.setStyleSheet( style );
     mainpage w;
     w.show();
     return a.exec();
